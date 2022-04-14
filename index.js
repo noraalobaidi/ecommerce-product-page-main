1a13707f477d79b58ec7886b545d6fe1ef67f3cd
noraalobaidi <nalobaidi_@hotmail.com>
2022-04-14 05:33:00 +0300

    MODIFIED files

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-document.getElementById("amount").innerHTML = "0"; //initilize product amount to 0
+//initilize product amount to 0
+document.getElementById("amount").innerHTML = 0;
+
+function removeOne(id) {
+  let amount = document.getElementById("amount").innerHTML;
+  if (amount > 0) {
+    amount--;
+    document.getElementById("amount").innerHTML = amount;
+  }
+}
+
+function addOne(id) {
+  let amount = document.getElementById("amount").innerHTML;
+  amount++;
+  document.getElementById("amount").innerHTML = amount;
+}
+
+function cartbox(id) {
+  var box = document.getElementById("show-cart");
+  if (box.className == "showCartnone") {
+    box.className = "showCart";
+  } else {
+    box.className = "showCartnone";
+  }
+}
