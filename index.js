f2c12e1ee3a8d2b708e028c195b8608bed07b0f0
noraalobaidi <nalobaidi_@hotmail.com>
2022-04-17 17:26:09 +0300

    FINISHED mobile website

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //initilize product amount to 0
 document.getElementById("amount").innerHTML = 0;
+document.getElementById("mobile-amount").innerHTML = 0;
 let cartElements = 0;
 let slide = 1;
+let mobileSlide = 1;
+let mobilecartElements = 0;
 
 //decrese amount of item by 1
 function removeOne() {
@@ -156,7 +159,7 @@ function changeimglightbox(id) {
     document.getElementById("lbthumb3").style.borderColor = "rgba(0, 0, 0, 0)";
     document.getElementById("lbthumb3").style.opacity = "100%";
   } else {
-    console.log("wrong");
+    // console.log("wrong");
   }
 }
 
@@ -208,4 +211,127 @@ function preimg() {
       slide--;
     }
   }
+}
+
+/////////////// mobile functions //////////////////
+function mobilenextimg() {
+  if (mobileSlide < 4) {
+    if (mobileSlide == 1) {
+      document.getElementById("mobile-img").src =
+        "../images/image-product-2.jpg";
+      mobileSlide++;
+    } else if (mobileSlide == 2) {
+      document.getElementById("mobile-img").src =
+        "../images/image-product-3.jpg";
+      mobileSlide++;
+    } else if (mobileSlide == 3) {
+      document.getElementById("mobile-img").src =
+        "../images/image-product-4.jpg";
+      mobileSlide++;
+    }
+  } else if (mobileSlide == 4) {
+    mobileSlide = 1;
+    document.getElementById("mobile-img").src = "../images/image-product-1.jpg";
+  }
+}
+
+function mobilepreimg() {
+  if (mobileSlide == 1) {
+    mobileSlide = 4;
+    document.getElementById("mobile-img").src = "../images/image-product-4.jpg";
+  } else if (mobileSlide <= 4) {
+    if (mobileSlide == 4) {
+      document.getElementById("mobile-img").src =
+        "../images/image-product-3.jpg";
+      mobileSlide--;
+    } else if (mobileSlide == 3) {
+      document.getElementById("mobile-img").src =
+        "../images/image-product-2.jpg";
+      mobileSlide--;
+    } else if (mobileSlide == 2) {
+      document.getElementById("mobile-img").src =
+        "../images/image-product-1.jpg";
+      mobileSlide--;
+    }
+  }
+}
+//decrese amount of item by 1
+function mobileremoveOne() {
+  let amount = document.getElementById("mobile-amount").innerHTML;
+  if (amount > 0) {
+    amount--;
+    document.getElementById("mobile-amount").innerHTML = amount;
+  }
+}
+//increase amount of item by 1
+function mobileaddOne() {
+  let amount = document.getElementById("mobile-amount").innerHTML;
+  amount++;
+  document.getElementById("mobile-amount").innerHTML = amount;
+}
+//show or hide cart box
+function mobilecartbox() {
+  var box = document.getElementById("mobile-show-cart");
+  if (box.className == "displaynone") {
+    box.className = "mobile-showCart";
+  } else {
+    box.className = "displaynone";
+  }
+}
+
+function mobiledeleteitem() {
+  document.getElementById("mobile-empty-cart").innerHTML =
+    "Your cart is empty.";
+  let itemInCart = document.getElementById("mobile-item-in-cart");
+  itemInCart.className = "displaynone";
+
+  mobilecartElements = 0;
+  var cartamountbox = document.getElementById("mobile-cart-amount"); //get cart amount box
+  cartamountbox.className = "displaynone";
+  document.getElementById("mobile-checkout").className = "displaynone";
+}
+
+function mobileaddToCart() {
+  let itemInCart = document.getElementById("mobile-item-in-cart");
+  let amount = document.getElementById("mobile-amount").innerHTML; //get amount
+  var cartamountbox = document.getElementById("mobile-cart-amount"); //get cart amount box
+  let getshoesprice = document
+    .getElementById("mobile-shoes-price")
+    .innerHTML.replace("$", ""); //get shoes price
+  let calctotalprice = Number(amount) * Number(getshoesprice); //calculate total price
+  if (mobilecartElements > 0 && amount > 0) {
+    //for cart not empty
+    mobilecartElements = mobilecartElements + Number(amount);
+    document.getElementById("mobile-cart-amount").innerHTML =
+      mobilecartElements;
+    document.getElementById("mobile-item-amount").innerHTML =
+      mobilecartElements;
+    let totalbefore = document.getElementById("mobile-total").innerHTML;
+    totalbefore = totalbefore.replace("$", "");
+    document.getElementById("mobile-total").innerHTML = `$${
+      calctotalprice + Number(totalbefore)
+    }`;
+    document.getElementById("mobile-amount").innerHTML = 0;
+  } else if (amount > 0) {
+    //for empty cart
+    document.getElementById("mobile-empty-cart").innerHTML = "";
+    mobilecartElements = mobilecartElements + Number(amount); //update cartelements
+    document.getElementById("mobile-cart-amount").innerHTML =
+      mobilecartElements; // get cart amount box
+
+    cartamountbox.className = "mobilecartAmount"; //display cart amount box by changing it class
+    document.getElementById("mobile-item-amount").innerHTML = amount;
+    document.getElementById("mobile-total").innerHTML = `$${calctotalprice}`;
+
+    itemInCart.className = "";
+    document.getElementById("mobile-checkout").className = "";
+    document.getElementById("mobile-amount").innerHTML = 0;
+  }
+}
+function closemenu() {
+  document.getElementById("mobile-menu").className = "displaynone";
+}
+
+function openmenu() {
+  document.getElementById("mobile-menu").className = "";
 }
